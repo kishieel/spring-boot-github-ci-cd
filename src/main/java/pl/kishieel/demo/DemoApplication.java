4f373d02d7fa22066c28b1eaa97a8f71ee07e698
Tomasz Kisiel <tkisiel5w4@yahoo.com>
2022-11-08 22:40:50 +0100

    Initial commit

---
+++ src/main/java/pl/kishieel/demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package pl.kishieel.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @RestController
+    public static class PingController {
+        @GetMapping("/ping")
+        public ResponseEntity<?> ping() {
+            return new ResponseEntity<>("pong", HttpStatus.OK);
+        }
+    }
+}
